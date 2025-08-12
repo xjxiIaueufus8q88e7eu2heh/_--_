7366fde86a67e15d65e7fe9c34621194f927b793
xjxiIaueufus8q88e7eu2heh <grdgfdhdhdiutuyfytdtw@gmail.com>
2025-08-12 14:35:47 +0530

    Update raw_socialblade.py

--- raw_socialblade.py
+++ raw_socialblade.py
@@ -1,38 +1,15 @@
 """Bypass bot-detection to view SocialBlade ranks for YouTube"""
 from seleniumbase import SB
-import time
-import os
-# Create unique filename based on current timestamp
-timestamp = int(time.time())
-screenshot_name = f"socialblade_{timestamp}.png"
 
-with SB(uc=True, test=True, ad_block=True, pls="none") as sb:
-    url = "https://socialblade.com/"
+with SB(uc=True, test=True, locale_code="en") as sb:
+    url = "https://guidedhacking.com/"
     sb.activate_cdp_mode(url)
-    sb.sleep(1.5)
-    if not sb.is_element_visible('input[placeholder*="Search"]'):
-        sb.uc_gui_click_captcha()
-        sb.sleep(0.5)
-    channel_name = "michaelmintz"
-    channel_title = "Michael Mintz"
-    sb.cdp.press_keys('input[placeholder*="Search"]', channel_name)
-    sb.sleep(2)
-    sb.cdp.click('a:contains("%s")' % channel_title)
-    sb.sleep(2)
-    sb.cdp.remove_elements("#lngtd-top-sticky")
-    sb.sleep(1.5)
-    name = sb.cdp.get_text("h3")
-    ch_name = name.split(" ")[-1]
-    name = name.split(" @")[0]
-    link = "https://www.youtube.com/%s" % ch_name
-    print("********** SocialBlade Stats for %s: **********" % name)
-    print(">>> (Link: %s) <<<" % link)
-    print(sb.get_text('[class*="grid lg:hidden"]'))
-    print("********** SocialBlade Ranks: **********")
-    print(sb.get_text('[class*="gap-3 flex-1"]'))
-    for i in range(17):
-        sb.cdp.scroll_down(6)
-        sb.sleep(0.1)
-    sb.sleep(2)
+    sb.sleep(5)
+    sb.uc_gui_click_captcha()
+#    sb.assert_text("Username", '[for="user_login"]', timeout=3)
+ #   sb.assert_element('label[for="user_login"]')
+ #   sb.highlight('button:contains("Sign in")')
+  #  sb.highlight('h1:contains("GitLab.com")')
+    sb.post_message("SeleniumBase wasn't detected", duration=4)
     os.system("ls -tgh; pwd; ls | grep -i *.png")
-    sb.save_screenshot("stack.png")
+    sb.save_screenshot("st2k.png")
